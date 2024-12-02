392df27a7583af92d7567142694d99b1fc86d674
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2024-12-02 15:30:04 +0000

    Menus in place

---
+++ Banking.Domain/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Domain
+{
+    public abstract class Account
+    {
+        public string AccountName { get; set; }
+
+        public Account(string firstName, string lastName)
+        {
+            AccountName = GenerateAccountName(firstName, lastName);
+        }
+
+        public string GenerateAccountName(string firstName, string lastName)
+        {
+            // Introduce the rules for creating account names here
+            throw new NotImplementedException();
+        }
+    }
+}
